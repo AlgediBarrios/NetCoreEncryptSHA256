c87a7c6122f12274f828927c7a00f6b069956a94
Algedi Barrios <59034229+AlgediBarrios@users.noreply.github.com>
2020-08-18 08:04:09 -0400

    NetCoreEncryptSHA256 Class
    Using Net Core you can Encrypt SHA256 a text and get back its Digital Sign

---
+++ EncryptSHA256.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleApiApp
+{
+    class EncryptSHA256
+    {
+        public static string GetSHA256(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+
+        public void Encode(string valor)
+        {
+           Console.WriteLine("Original Value:" + valor + "====>Digital Sign:" +  GetSHA256(valor));
+        }
+    }
+}
